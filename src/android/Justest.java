4faa52b527d2694acce9ab014cb19ac72bb7f5de
Mansur <988302229@sms.tj>
2020-07-20 14:14:29 +0500

    ff

---
+++ src/android/Justest.java
@@ -0,0 +1,33 @@
+package just.test;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.content.Context;
+import android.widget.Toast;
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class Justest extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if ("show".equals(action)) {
+            show(args.getString(0), callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+	private void show(String msg, CallbackContext callbackContext) {
+		if (msg == null || msg.length() == 0) {
+			callbackContext.error("Empty message!");
+		} else {
+			Toast.makeText(webView.getContext(), msg, Toast.LENGTH_LONG).show();
+			callbackContext.success(msg);
+		}
+    }
+}
